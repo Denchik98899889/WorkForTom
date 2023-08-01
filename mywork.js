e3cdb97081a2983bf2fa0d9f970ba41841ba9137
Denchik98899889 <120317955+Denchik98899889@users.noreply.github.com>
2023-08-01 14:23:57 +0300

    Add files via upload

--- mywork.js
+++ mywork.js
@@ -1,18 +1,28 @@
-const input = document.getElementById('inputtt')
-
-const btn = document.getElementById('btn')
-
-const result = document.getElementById('result')
-
-
-btn.addEventListener('click' , (denis) => {
-
-
-    result.innerHTML +=  `<p>
-
-    ${input.value}
-    
-        </p>`
-    
-    
-}) 
+// Функція для додавання нового завдання
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+  
+    const taskList = document.getElementById('taskList');
+    const newTask = document.createElement('li');
+    newTask.innerHTML = `
+      <input type="checkbox" onchange="toggleTask(this)">
+      <span>${taskText}</span>
+      <button onclick="deleteTask(this)">Видалити</button>
+    `;
+    taskList.appendChild(newTask);
+    taskInput.value = '';
+  }
+  
+  // Функція для відмітки завдання як виконаного або невиконаного
+  function toggleTask(checkbox) {
+    const taskText = checkbox.nextElementSibling;
+    taskText.classList.toggle('completed', checkbox.checked);
+  }
+  
+  // Функція для видалення завдання
+  function deleteTask(button) {
+    const taskItem = button.parentElement;
+    taskItem.remove();
+  }
